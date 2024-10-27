97d25e0b68acd900dc4d9d6092c340b7e18296cb
kwon1232 <126114777+kwon1232@users.noreply.github.com>
2024-10-27 22:08:57 +0900

    Feat. Server Test
    문제없음

--- Source/Multi/PuzzlePlatformGameInstance.cpp
+++ Source/Multi/PuzzlePlatformGameInstance.cpp
@@ -12,3 +12,30 @@ void UPuzzlePlatformGameInstance::Init()
 {
 	UE_LOG(LogTemp, Warning, TEXT("GameInstance Init"));
 }
+
+void UPuzzlePlatformGameInstance::Host()
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+
+	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, TEXT("Hosting"));
+	
+	UWorld* World = GetWorld();
+	if (ensure(World != nullptr)) return;
+
+	World->ServerTravel("/Game/ThirdPerson/Maps/ThirdPersonMap");
+}
+
+void UPuzzlePlatformGameInstance::Join(const FString& Address)
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+
+	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, FString::Printf(TEXT("Joining %s"), *Address));
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (!ensure(PlayerController != nullptr)) return;
+
+	PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
+}
+
--- Source/Multi/PuzzlePlatformGameInstance.h
+++ Source/Multi/PuzzlePlatformGameInstance.h
@@ -18,4 +18,10 @@ public:
 	UPuzzlePlatformGameInstance(const FObjectInitializer & objectInitializer);
 	
 	virtual void Init();
+
+	UFUNCTION(Exec)
+	void Host();
+
+	UFUNCTION(Exec)
+	void Join(const FString& Address);
 };
